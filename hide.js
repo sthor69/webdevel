3312379f9e63f15b5d5c578c240ae61fbd764eac
Sergio Torassa <example@hotmail.com>
2013-12-04 10:48:51 +0000

    first javascript and css

---
+++ hide.js
@@ -0,0 +1,12 @@
+/* save the 2 nodes to variables */
+var button = document.getElementById("hide"),
+    target = document.getElementById("target");
+
+/* define what we want to do in a function */
+function hide(){
+   target.setAttribute("class","hide");
+}
+
+/* add the CSS class when the button is clicked */
+button.addEventListener("click", hide, false);
+
--- index.php
+++ index.php
@@ -1,8 +1,20 @@
-<html>
+<!doctype html>
+<html lang="en">
   <head>
+    <title>Click to Hide</title>
+    <meta charset="utf-8">
+<!-- css -->
+    <link rel="stylesheet" href="css/hide.css">
   </head>
-
   <body>
-    <p> asldjaklsdjklajhskdja </p>
+    <button type="button" id="hide">Hide the text</button>
+    <div id="target">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dolor metus,
+      sagittis et aliquam et, ornare et libero. Etiam eu nisi felis, ac posuere metus.
+      Vivamus molestie bibendum imperdiet. Etiam et faucibus metus.</p>
+    </div><!--/#target-->
+    <!-- javascript -->
+    <script src="js/hide.js"></script>
   </body>
-</head>
+</html>
+
